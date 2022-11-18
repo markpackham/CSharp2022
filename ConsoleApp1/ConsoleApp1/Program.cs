e109fe0938b95fe93ffc83bf1c3ba94070d08209
mark <markpackham1@gmail.com>
2022-11-18 10:32:09 +0000

    change console color
    https://youtu.be/M5ugY7fWydE?t=542

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,5 +2,14 @@
 
 namespace ConsoleApp1
 {
-
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Hello World");
+        }
+    }
 }
