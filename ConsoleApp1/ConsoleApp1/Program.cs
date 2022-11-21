06ee060da0e41ee8ff1ce33b201780d9ce6a40f1
mark <markpackham1@gmail.com>
2022-11-21 16:13:46 +0000

    demo array types
    https://youtu.be/M5ugY7fWydE?t=2932

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,11 @@ namespace ConsoleApp1
             int[] favNums = new int[3];
             favNums[0] = 42;
             Console.WriteLine(favNums[0]);
+            string[] customers = { "Bob","Alice","Sue" };
+            var employees = new[] { "Mike", "Bill", "John" };
+            object[] randomArray = { "Paul", 45, 1.234};
+            Console.WriteLine(randomArray[2].GetType());
+            Console.WriteLine(randomArray.Length);
         }
     }
 }
