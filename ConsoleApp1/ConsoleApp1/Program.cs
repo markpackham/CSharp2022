929ec54e763f876ec7682bc41c6ef1e5685708ac
mark <markpackham1@gmail.com>
2022-11-21 16:07:34 +0000

    demo array
    https://youtu.be/M5ugY7fWydE?t=2770

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string randString = "This is a string";
-            Console.WriteLine("String length :{0}", randString.Length);
-            Console.WriteLine("String Contains is :{0}", randString.Contains("is"));
-            Console.WriteLine("Index of 'is' is :{0}", randString.IndexOf("is"));
-            Console.WriteLine("Remove String: {0}", randString.Remove(10,6));
-            Console.WriteLine("Insert String :{0}", randString.Insert(10,"short "));
-            Console.WriteLine("Replace string :{0}", randString.Replace("string","twine"));
-
+            int[] favNums = new int[3];
+            favNums[0] = 42;
+            Console.WriteLine(favNums[0]);
         }
     }
 }
