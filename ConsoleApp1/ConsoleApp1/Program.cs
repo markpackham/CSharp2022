cbe6d15549d13a8ad0d3ec92ecc1cea943aeb9d3
mark <markpackham1@gmail.com>
2022-11-21 16:44:35 +0000

    output multi dimensional array
    https://youtu.be/M5ugY7fWydE?t=3367

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,6 +14,22 @@ namespace ConsoleApp1
             object[] randomArray = { "Paul", 45, 1.234};
             Console.WriteLine(randomArray[2].GetType());
             Console.WriteLine(randomArray.Length);
+            for(int j = 0; j < randomArray.Length; j++)
+            {
+                Console.WriteLine("Index:{0} - {1}", j, randomArray[j]);
+            }
+
+            Console.WriteLine("--------------");
+            string[,] customerNames = new string[2, 2] { { "Bob","Smith"},{ "Sally","Jones"} };
+            Console.WriteLine(customerNames[0,0]);
+            for (int j = 0; j < customerNames.GetLength(0); j++)
+            {
+                for (int k = 0; k < customerNames.GetLength(0); k++)
+                {
+                    Console.WriteLine("Index:{0}", customerNames[j,k]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
