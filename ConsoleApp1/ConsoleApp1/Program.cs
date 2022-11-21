384486a3eddf1e6fe4eb4d2a2ee17489a477160f
mark <markpackham1@gmail.com>
2022-11-21 13:39:02 +0000

    demo implicit (lossless) conversation and explicit conversion (loss)
    https://youtu.be/M5ugY7fWydE?t=1713

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            bool canIVote = true;
+            bool boolFromStr = bool.Parse("true");
+            int intFromStr = int.Parse("100");
+            double dblFromStr = double.Parse("1.234");
+
+            string strVal = dblFromStr.ToString();
+            Console.WriteLine($"Data type: {strVal.GetType()}");
+            double dblNum = 12.345;
+            Console.WriteLine($"Int: {(int)dblNum}");
+            int intNum = 10;
+            long longNum = intNum;
         }
     }
 }
