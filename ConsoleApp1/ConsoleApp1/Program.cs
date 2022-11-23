c43a009a3e86509a7f019ae51441966e4506e041
mark <markpackham1@gmail.com>
2022-11-23 16:22:30 +0000

    demo use of "params" for multiple parameters
    https://youtu.be/M5ugY7fWydE?t=6845

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,19 +7,14 @@ namespace ConsoleApp1
     public class Program
     {
         // --- Functions --- //
-        public static void NonRefSwap(int num3, int num4)
+        static double GetSomeMore(params double[] nums)
         {
-            int temp = num3;
-            num3 = num4;
-            num4 = temp;
-        }
-
-
-        public static void Swap(ref int num3, ref int num4)
-        {
-            int temp = num3;
-            num3 = num4;
-            num4 = temp;
+            double sum = 0;
+            foreach (int i in nums)
+            {
+                sum += i;
+            }
+            return sum;
         }
         
 
@@ -27,13 +22,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            int num3 = 10;
-            int num4 = 20;
-            Console.WriteLine("Before Swap num1: {0} num2: {1}",num3, num4);
-            NonRefSwap(num3, num4);
-            Console.WriteLine("NonRefSwap num1: {0} num2: {1}", num3, num4);
-            Swap(ref num3, ref num4);
-            Console.WriteLine("After Swap num1: {0} num2: {1}", num3, num4);
+            Console.WriteLine("{0}", GetSomeMore(1, 2, 3, 4, 5));
         }
     }
 }
