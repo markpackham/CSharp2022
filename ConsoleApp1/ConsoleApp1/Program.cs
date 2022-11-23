7fbcd95512c2f53a008eab84abf2135559880666
mark <markpackham1@gmail.com>
2022-11-23 16:42:19 +0000

    add days, months and years to date
    https://youtu.be/M5ugY7fWydE?t=7325

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,6 +15,10 @@ namespace ConsoleApp1
         {
             DateTime awesomeDate = new DateTime(1974, 12, 21);
             Console.WriteLine("Day of the Week: {0}", awesomeDate.DayOfWeek);
+            awesomeDate = awesomeDate.AddDays(4);
+            awesomeDate = awesomeDate.AddMonths(1);
+            awesomeDate = awesomeDate.AddYears(1);
+            Console.WriteLine("New Date: {0}", awesomeDate.Date);
         }
     }
 }
