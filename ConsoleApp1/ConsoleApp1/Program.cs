a8439a4dd881d7d9f2b3aa641322c57343fb55ad
mark <markpackham1@gmail.com>
2022-11-23 16:39:56 +0000

    show day of the week
    https://youtu.be/M5ugY7fWydE?t=7253

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,24 +7,14 @@ namespace ConsoleApp1
     public class Program
     {
         // --- Functions --- //
-        static double GetSum2(double x = 1, double y =1)
-        {
-            return x + y;
-        }
 
-        static double GetSum2(string x = "1", string y = "1")
-        {
-            double dblX = Convert.ToDouble(x);
-            double dblY = Convert.ToDouble(y);
-            return dblX + dblY;
-        }
 
         // --- End of Functions --- //
 
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}",GetSum2(1,2));
-            Console.WriteLine("{0}",GetSum2("1","2"));
+            DateTime awesomeDate = new DateTime(1974, 12, 21);
+            Console.WriteLine("Day of the Week: {0}", awesomeDate.DayOfWeek);
         }
     }
 }
