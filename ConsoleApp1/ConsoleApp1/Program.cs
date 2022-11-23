6dbc9842b56078a3638761de270390df7208252c
mark <markpackham1@gmail.com>
2022-11-23 16:46:09 +0000

    show timespan
    https://youtu.be/M5ugY7fWydE?t=7440

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,6 +19,9 @@ namespace ConsoleApp1
             awesomeDate = awesomeDate.AddMonths(1);
             awesomeDate = awesomeDate.AddYears(1);
             Console.WriteLine("New Date: {0}", awesomeDate.Date);
+            TimeSpan lunchTime = new TimeSpan(12,30,0,0);
+            lunchTime = lunchTime.Subtract(new TimeSpan(0,15,0));
+            Console.WriteLine("New Time: {0}", lunchTime.ToString());
         }
     }
 }
