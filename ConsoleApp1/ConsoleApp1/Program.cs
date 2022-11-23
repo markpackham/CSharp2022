6357ad1791578a0c125533fd2964fec345bc9657
mark <markpackham1@gmail.com>
2022-11-23 16:37:01 +0000

    demo method overloading
    https://youtu.be/M5ugY7fWydE?t=7184

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,16 +7,24 @@ namespace ConsoleApp1
     public class Program
     {
         // --- Functions --- //
-        static void PrintInfo(string name, int zipCode)
+        static double GetSum2(double x = 1, double y =1)
         {
-            Console.WriteLine("{0} {1}", name, zipCode);
+            return x + y;
+        }
+
+        static double GetSum2(string x = "1", string y = "1")
+        {
+            double dblX = Convert.ToDouble(x);
+            double dblY = Convert.ToDouble(y);
+            return dblX + dblY;
         }
 
         // --- End of Functions --- //
 
         static void Main(string[] args)
         {
-            PrintInfo(zipCode:123,name:"Jimmy");
+            Console.WriteLine("{0}",GetSum2(1,2));
+            Console.WriteLine("{0}",GetSum2("1","2"));
         }
     }
 }
