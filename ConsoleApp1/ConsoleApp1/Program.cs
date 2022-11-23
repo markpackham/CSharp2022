330d600232518a185b370f7829f49390febf17a0
mark <markpackham1@gmail.com>
2022-11-23 16:18:46 +0000

    demo use of "ref" for params
    https://youtu.be/M5ugY7fWydE?t=6699

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,19 +7,33 @@ namespace ConsoleApp1
     public class Program
     {
         // --- Functions --- //
-        
-        static void DoubleIt(int x, out int solution)
+        public static void NonRefSwap(int num3, int num4)
         {
-            solution = x * 2;
+            int temp = num3;
+            num3 = num4;
+            num4 = temp;
         }
+
+
+        public static void Swap(ref int num3, ref int num4)
+        {
+            int temp = num3;
+            num3 = num4;
+            num4 = temp;
+        }
+        
 
         // --- End of Functions --- //
 
         static void Main(string[] args)
         {
-            int solution;
-            DoubleIt(15, out solution);
-            Console.WriteLine("15 * 2 = {0}", solution);
+            int num3 = 10;
+            int num4 = 20;
+            Console.WriteLine("Before Swap num1: {0} num2: {1}",num3, num4);
+            NonRefSwap(num3, num4);
+            Console.WriteLine("NonRefSwap num1: {0} num2: {1}", num3, num4);
+            Swap(ref num3, ref num4);
+            Console.WriteLine("After Swap num1: {0} num2: {1}", num3, num4);
         }
     }
 }
