2aa7b9adad8841c2967fed6b010709dd982f0021
mark <markpackham1@gmail.com>
2022-11-24 12:35:03 +0000

    add AnimalIDInfo props
    https://youtu.be/M5ugY7fWydE?t=10387

--- DogInherit/DogInherit/AnimalIDInfo.cs
+++ DogInherit/DogInherit/AnimalIDInfo.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace DogInherit
 {
-    internal class AnimalIDInfo
+    class AnimalIDInfo
     {
+        public int IDNum { get; set; } = 0;
+        public string Owner { get; set; } = "No Owner";
     }
 }
