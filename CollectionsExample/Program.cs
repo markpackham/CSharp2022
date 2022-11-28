e27bd6950cf6e685b177677227cb1757f2113713
mark <markpackham1@gmail.com>
2022-11-28 12:04:07 +0000

    show array list count
    https://youtu.be/M5ugY7fWydE?t=16083

--- CollectionsExample/Program.cs
+++ CollectionsExample/Program.cs
@@ -3,6 +3,13 @@ using System.Collections;
 // used for dictionaries
 using System.Collections.Generic;
 
-#region ArrayList
+#region ArrayList Code
+
+ArrayList arrayList= new ArrayList();
+
+arrayList.Add("Billy");
+arrayList.Add(40);
+Console.WriteLine($"Count {arrayList.Count}");
+
 
 #endregion
