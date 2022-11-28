e3b650e4b83516b9f64e6e90c0956bc27157bbe9
mark <markpackham1@gmail.com>
2022-11-28 09:52:49 +0000

    demo if Vehicle is drivable
    https://youtu.be/M5ugY7fWydE?t=13095

--- InterfaceExamples/InterfaceExamples/Program.cs
+++ InterfaceExamples/InterfaceExamples/Program.cs
@@ -1,1 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using InterfaceExamples;
+
+Vehicle buick = new Vehicle("Buick",4,160);
+
+if(buick is IDrivable)
+{
+    buick.Move();
+    buick.Stop();
+}
+else
+{
+    Console.WriteLine("The {0} is faulty and can't be driven", buick.Brand);
+}
