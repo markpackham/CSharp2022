a2df540a144131b967bb3698750469244a35c5c9
mark <markpackham1@gmail.com>
2022-11-28 12:01:53 +0000

    demo collapsible "region" and "endregion"
    https://youtu.be/M5ugY7fWydE?t=16004

---
+++ CollectionsExample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+// used for dictionaries
+using System.Collections.Generic;
+
+#region ArrayList
+
+#endregion
