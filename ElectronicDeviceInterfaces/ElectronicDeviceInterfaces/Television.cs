318910cfe1dd3b84ad7eae9a82786aa9a5d7e2dd
mark <markpackham1@gmail.com>
2022-11-28 10:06:33 +0000

    set the TV volume
    https://youtu.be/M5ugY7fWydE?t=13495

--- ElectronicDeviceInterfaces/ElectronicDeviceInterfaces/Television.cs
+++ ElectronicDeviceInterfaces/ElectronicDeviceInterfaces/Television.cs
@@ -8,6 +8,34 @@ namespace ElectronicDeviceInterfaces
 {
     class Television : IElectronicDevice
     {
+        public int Volume { get; set; }
 
+        public void Off()
+        {
+            Console.WriteLine("The TV is Off");
+        }
+
+        public void On()
+        {
+            Console.WriteLine("The TV is On");
+        }
+
+        public void VolumeDown()
+        {
+            if(Volume != 0)
+            {
+                Volume--;
+                Console.WriteLine($"The TV is at {Volume}");
+            }
+        }
+
+        public void VolumeUp()
+        {
+            if (Volume != 100)
+            {
+                Volume++;
+                Console.WriteLine($"The TV is at {Volume}");
+            }
+        }
     }
 }
