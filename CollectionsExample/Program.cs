80179742eab5f1ef0a3afccc3faa5d275efce1fe
mark <markpackham1@gmail.com>
2022-11-28 12:10:39 +0000

    demo array list methods
    https://youtu.be/M5ugY7fWydE?t=16261

--- CollectionsExample/Program.cs
+++ CollectionsExample/Program.cs
@@ -6,10 +6,21 @@ using System.Collections.Generic;
 #region ArrayList Code
 
 ArrayList arrayList= new ArrayList();
+ArrayList arrayList2= new ArrayList();
 
 arrayList.Add("Billy");
 arrayList.Add(40);
 Console.WriteLine($"Count {arrayList.Count}");
+Console.WriteLine($"Capacity {arrayList.Capacity}");
 
+arrayList2.AddRange(new object[] {"Jake","Neil","Jim","Zeuz","Joe","James","Xenu","Jeff","Jahn","Zena" });
+arrayList.AddRange(arrayList2);
+
+arrayList2.Sort();
+arrayList2.Reverse();
+
+arrayList.Insert(1, "Turkey");
+arrayList2.RemoveAt(0);
+arrayList2.RemoveRange(0, 1);
 
 #endregion
