3a04b02bbea053757ca702fcf73a50d55192d3bf
mark <markpackham1@gmail.com>
2022-11-28 10:52:08 +0000

    report on health loss
    https://youtu.be/M5ugY7fWydE?t=14869

--- ThorHulkFight/ThorHulkFight/Battlecs.cs
+++ ThorHulkFight/ThorHulkFight/Battlecs.cs
@@ -21,11 +21,34 @@ namespace ThorHulkFight
 
             double dmg2WarriorB = warriorAAttackAmt - warriorBBlockAmt;
 
+            if(dmg2WarriorB> 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarriorB;
+            }
+            else
+            {
+                dmg2WarriorB = 0;
+            }
+
+            Console.WriteLine($"{warriorA.Name} Attacks {warriorB.Name} and Deals {dmg2WarriorB} Damge");
+            Console.WriteLine($"{warriorB.Name} Has {warriorB.Health} Health\n");
+
             double warriorBAttackAmt = warriorB.Attack();
             double warriorABlockAmt = warriorA.Block();
 
             double dmg2WarriorA = warriorBAttackAmt - warriorABlockAmt;
 
+            if (dmg2WarriorA > 0)
+            {
+                warriorA.Health = warriorA.Health - dmg2WarriorA;
+            }
+            else
+            {
+                dmg2WarriorA = 0;
+            }
+
+            Console.WriteLine($"{warriorB.Name} Attacks {warriorA.Name} and Deals {dmg2WarriorA} Damge");
+            Console.WriteLine($"{warriorA.Name} Has {warriorA.Health} Health\n");
 
         }
     }
