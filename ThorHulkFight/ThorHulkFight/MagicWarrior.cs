736879a4ec967f5a27b17d97f8b762149e73757a
mark <markpackham1@gmail.com>
2022-11-28 11:53:18 +0000

    account for teleport failure
    https://youtu.be/M5ugY7fWydE?t=15781

--- ThorHulkFight/ThorHulkFight/MagicWarrior.cs
+++ ThorHulkFight/ThorHulkFight/MagicWarrior.cs
@@ -8,5 +8,30 @@ namespace ThorHulkFight
 {
     internal class MagicWarrior : Warrior
     {
+        int teleportChance = 0;
+        CanTeleport teleportType = new CanTeleport();
+
+        public MagicWarrior(string name = "Warrior", double health = 0, double attackMax = 0, double blockMax = 0, int teleportChance = 0) : base(name,health,attackMax,blockMax)
+        {
+            this.teleportChance = teleportChance;
+        }
+
+        public override double Block()
+        {
+            Random rnd= new Random();
+            int rndDodge = rnd.Next(1, 100);
+
+            if (rndDodge < this.teleportChance)
+            {
+                Console.WriteLine($"{Name} {teleportType.teleport()}");
+                // return a huge number which nullifies any damage
+                return 100000;
+            }
+            else
+            {
+                // do this if a teleport fails
+                return base.Block();
+            }
+        }
     }
 }
