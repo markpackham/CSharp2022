ce33887ae11a0911ab0e7df5b63b318ccda35288
mark <markpackham1@gmail.com>
2022-11-28 10:21:46 +0000

    layout plans idea for Hulk vs Thor fight
    https://youtu.be/M5ugY7fWydE?t=13963

---
+++ ThorHulkFight/ThorHulkFight/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ThorHulkFight;
+
+/*
+Thor Attacks Hulk and Deals 74 Damage
+Maximus Has 69 Health
+
+Hulk Attacks Thor and Deals 6 Damage
+Bob Has 6 Health
+
+Thor Attacks Hulk and Deals 48 Damage
+Maximus Has 21 Health
+
+Hulk Attacks Thor and Deals 48 Damage
+Bob Has -42 Health
+
+Thor has Died and Hulk is Victorious
+
+Game Over
+*/
+
+// thor = new Warror("Thor",100,26,10);
+// hulk = new Warror("Hulk",100,26,10);
+// Battle.StartFight(thor,hulk);
