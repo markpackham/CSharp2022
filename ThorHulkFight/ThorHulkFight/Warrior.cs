45643199a1b985eed171a9bf9af62e3974e04359
mark <markpackham1@gmail.com>
2022-11-28 10:28:14 +0000

    add Warrior constructor
    https://youtu.be/M5ugY7fWydE?t=14166

--- ThorHulkFight/ThorHulkFight/Warrior.cs
+++ ThorHulkFight/ThorHulkFight/Warrior.cs
@@ -8,5 +8,22 @@ namespace ThorHulkFight
 {
     internal class Warrior
     {
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double AttackMax { get; set; }
+        public double BlockMax { get; set; }
+
+        Random rnd = new Random();
+
+        public Warrior(string name = "Warrior", 
+            double health = 0, 
+            double attackMax = 0, 
+            double blockMax = 0)
+        {
+            Name = name;
+            Health = health;
+            AttackMax = attackMax;
+            BlockMax = blockMax;
+        }
     }
 }
