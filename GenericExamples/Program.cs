36d90f6bf00d6ad63596979fec06d9d91b09ce83
mark <markpackham1@gmail.com>
2022-11-29 12:30:23 +0000

    Add, Insert and Remove animals
    https://youtu.be/M5ugY7fWydE?t=17627

--- GenericExamples/Program.cs
+++ GenericExamples/Program.cs
@@ -2,3 +2,16 @@
 using System.Collections.Generic;
 
 using GenericExamples;
+
+List<Animal> animalList = new List<Animal>();
+
+List<int> numList = new List<int>();
+numList.Add(100);
+
+animalList.Add(new Animal() { Name = "Snarf" });
+animalList.Add(new Animal() { Name = "Whiskers" });
+animalList.Add(new Animal() { Name = "Rover" });
+
+// use Insert to place at a specific position
+animalList.Insert(1,new Animal() { Name = "Lino" });
+animalList.RemoveAt(1);
