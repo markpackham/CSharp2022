09ea412e33bdc115662ebcd0ab6183c326568745
mark <markpackham1@gmail.com>
2022-11-30 17:37:40 +0000

    demo use of Lambda function for even only numbers
    https://youtu.be/M5ugY7fWydE?t=18756

---
+++ DelegateLambdaExample/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DelegateLambdaExample
+{
+
+    class Program
+    {
+        // Delegate that is assigned a Lambda function
+        delegate double doubleIt(double val);
+
+        static void Main(string[] args)
+        {
+            // Lambda example
+            doubleIt dblIt = x => x * 2;
+            Console.WriteLine($"5 * 2 = {dblIt(5)}");
+
+            List<int> numList = new List<int> { 1,2,3,4,5,6,7,8};
+            // Lambda for only even numbers
+            var evenList = numList.Where(a => a % 2 == 0).ToList();
+            Console.WriteLine("Even numbers");
+            foreach(var j in evenList)
+            {
+                Console.WriteLine(j);
+            }
+        }
+
+    }
+
+}
