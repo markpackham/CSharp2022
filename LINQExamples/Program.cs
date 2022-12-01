5439eb9afb51adc55d959a13cba11a767cf40c78
mark <markpackham1@gmail.com>
2022-12-01 17:50:15 +0000

    demo LINQ with Select and Contains
    https://youtu.be/M5ugY7fWydE?t=21136

---
+++ LINQExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LINQExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ reminds me a lot of SQL
+
+            string[] dogs = { "rover", "lassie", "k9", "scooby doo", "old yeller", "snoopy", "rin tin tin" };
+
+            // find dogs that have spaces in their name
+            var dogSpaces = from dog in dogs
+                            where dog.Contains(" ")
+                            orderby dog descending
+                            select dog;
+
+            foreach (var dog in dogSpaces)
+            {
+                Console.WriteLine(dog);
+            }
+        }
+    }
+}
