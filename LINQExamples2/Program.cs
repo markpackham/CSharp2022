63701aca5cef2194d03d964f476e9092739d4dc6
mark <markpackham1@gmail.com>
2022-12-01 23:00:51 +0000

    create animals
    https://youtu.be/M5ugY7fWydE?t=21602

---
+++ LINQExamples2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace LINQExamples2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList famAnimals = new ArrayList()
+            {
+                new Animal{Name = "Heidy", Height = 10, Weight = 10},
+                new Animal{Name = "Pongo", Height = 11, Weight = 11},
+                new Animal{Name = "Fifi", Height = 12, Weight = 12},
+                new Animal{Name = "Jasper", Height = 13, Weight = 13},
+            };
+
+            var famAnimalEnum = famAnimals.OfType<Animal>();
+        }
+    }
+}
