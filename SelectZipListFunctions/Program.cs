dff307b604fb667fd83f71e2ad35665edccc5977
mark <markpackham1@gmail.com>
2022-12-01 09:37:53 +0000

    demo list range of 1 to 10 squared
    https://youtu.be/M5ugY7fWydE?t=19169

---
+++ SelectZipListFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿// maintain old style main Program style
+// https://learn.microsoft.com/en-gb/dotnet/core/tutorials/top-level-templates#use-the-old-program-style
+
+namespace SelectZipListFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var oneTo10 = new List<int>();
+            oneTo10.AddRange(Enumerable.Range(1, 10));
+            var squares = oneTo10.Select(x => x * x);
+            foreach(var square in squares)
+            {
+                Console.WriteLine(square);
+            }
+        }
+    }
+}
