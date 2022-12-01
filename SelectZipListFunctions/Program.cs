106f324fe97503202375ca6375a7ba5abf5ccd10
mark <markpackham1@gmail.com>
2022-12-01 16:01:38 +0000

    finish off List methods
    https://youtu.be/M5ugY7fWydE?t=19676

--- SelectZipListFunctions/Program.cs
+++ SelectZipListFunctions/Program.cs
@@ -35,6 +35,68 @@ namespace SelectZipListFunctions
                 Console.WriteLine(item);
             }
 
+            // ---------- Aggregate/Sum ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("Aggregate/Sum");
+            var numList2 = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Sum {0}", numList2.Aggregate((a,b)=>a + b));
+
+
+            // ---------- Average ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("Average");
+            var numList3 = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("AVG : {0}",
+                numList3.AsQueryable().Average());
+
+            // ---------- All meeting condition ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("All meeting a condition (eg > or <)");
+
+            var numList4 = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("All > 3 : {0}",
+                numList4.All(x => x > 3));
+
+            // ---------- Any meeting conditon ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("Any meeting conditon");
+
+            var numList5 = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Any > 3 : {0}",
+                numList5.Any(x => x > 3));
+
+            // ---------- Distinct ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("Distinct - purge duplicates");
+
+            var numList6 = new List<int>() { 1, 2, 3, 2, 3 };
+
+            Console.WriteLine("Distinct : {0}",
+                string.Join(", ", numList6.Distinct()));
+
+            // ---------- Except ----------
+            Console.WriteLine("-------------");
+            Console.WriteLine("Except - return all values except exceptions");
+
+            var numList7 = new List<int>() { 1, 2, 3, 2, 3 };
+            var numList8 = new List<int>() { 3 };
+
+            Console.WriteLine("Except : {0}",
+                string.Join(", ", numList7.Except(numList8)));
+
+            // ---------- Intersect ----------
+            // Receives 2 lists and returns values that both lists have
+            Console.WriteLine("-------------");
+            Console.WriteLine("Intersect - opposite of Except");
+            var numList9 = new List<int>() { 1, 2, 3, 2, 3 };
+            var numList10 = new List<int>() { 2, 3 };
+
+            Console.WriteLine("Intersect : {0}",
+                string.Join(", ", numList9.Intersect(numList10)));
+
         }
     }
 }
