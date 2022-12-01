ee419b0dee04c895bd1ab40b00dcb6ba132b1af0
mark <markpackham1@gmail.com>
2022-12-01 16:19:15 +0000

    add Box class
    https://youtu.be/M5ugY7fWydE?t=20180

---
+++ IEnumeratorExample/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorExample
+{
+    internal class Box
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Breadth { get; set; }
+
+        public Box():this(1,1,1) { }
+
+        public Box(double length, double width, double breadth)
+        {
+            Length = length;
+            Width = width;
+            Breadth = breadth;
+        }
+    }
+}
