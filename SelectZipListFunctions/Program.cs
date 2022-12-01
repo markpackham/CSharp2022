22873a7ee74c35837c92f9be01798b3a88933edb
mark <markpackham1@gmail.com>
2022-12-01 09:46:44 +0000

    demo zip in list
    https://youtu.be/M5ugY7fWydE?t=19292

--- SelectZipListFunctions/Program.cs
+++ SelectZipListFunctions/Program.cs
@@ -7,6 +7,12 @@ namespace SelectZipListFunctions
     {
         static void Main(string[] args)
         {
+
+            // ---------- SELECT ----------
+            // Select allows us to execute a function 
+            // on each item in a list
+            Console.WriteLine("-------------");
+            Console.WriteLine("SELECT");
             var oneTo10 = new List<int>();
             oneTo10.AddRange(Enumerable.Range(1, 10));
             var squares = oneTo10.Select(x => x * x);
@@ -14,6 +20,21 @@ namespace SelectZipListFunctions
             {
                 Console.WriteLine(square);
             }
+
+            // ---------- ZIP ----------
+            // Zip applies a function to two lists (nothing to do with zip files or zip codes)
+            // Add values in 2 lists together
+            // so {1,3,4} + {4,6,8} = {5,9,12}
+            Console.WriteLine("-------------");
+            Console.WriteLine("ZIP");
+            var listOne = new List<int>(new int[] {1,3,4});
+            var listTwo = new List<int>(new int[] {4,6,8});
+            var sumList = listOne.Zip(listTwo, (x, y) => x + y).ToList();
+            foreach(var item in sumList)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
