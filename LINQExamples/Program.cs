f158a5451f220b23d321d175acae9698ecc097ef
mark <markpackham1@gmail.com>
2022-12-01 18:17:11 +0000

    find array items greater than 20
    https://youtu.be/M5ugY7fWydE?t=21321

--- LINQExamples/Program.cs
+++ LINQExamples/Program.cs
@@ -2,6 +2,25 @@
 {
     internal class Program
     {
+
+        static int[] QueryIntArray()
+        {
+            int[] nums = {5,10,15,20,25,35};
+
+            var gt20 = from num in nums
+                       where num > 20
+                       orderby num
+                       select num;
+
+            foreach(int num in gt20)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine($"Get Type: {gt20.GetType()}");
+
+            var listGT20 = gt20.ToList<int>();
+        }
+
         static void Main(string[] args)
         {
             // LINQ reminds me a lot of SQL
@@ -18,6 +37,8 @@
             {
                 Console.WriteLine(dog);
             }
+
+
         }
     }
 }
