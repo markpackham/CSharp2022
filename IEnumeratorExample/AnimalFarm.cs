5a25150596316cef18ba179c5708e077b28d3a05
mark <markpackham1@gmail.com>
2022-12-01 16:05:08 +0000

    implement IEnumerable
    https://youtu.be/M5ugY7fWydE?t=19793

---
+++ IEnumeratorExample/AnimalFarm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorExample
+{
+    internal class AnimalFarm : IEnumerable<int>
+    {
+        private List<Animal> animalList = new List<Animal>();
+
+        public AnimalFarm(List<Animal> animalList)
+        {
+            this.animalList = animalList;
+        }
+
+        public AnimalFarm() { }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
