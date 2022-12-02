f170b6caeea4edc5a8727a8059d73881e6156138
mark <markpackham1@gmail.com>
2022-12-02 11:30:02 +0000

    set threads to issue withdrawls
    https://youtu.be/M5ugY7fWydE?t=23468

--- ThreadLockExample/Program.cs
+++ ThreadLockExample/Program.cs
@@ -7,6 +7,14 @@
             BankAccount account = new BankAccount(10);
 
             Thread.CurrentThread.Name = "main";
+            Thread[] threads = new Thread[15];
+
+            for (int i= 0; i<15; i++)
+            {
+                Thread t = new Thread(new ThreadStart(account.IssueWithdraw));
+                t.Name = i.ToString();
+                threads[i] = t;
+            }
         }
     }
 }
