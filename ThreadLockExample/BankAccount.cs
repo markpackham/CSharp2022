8dcb1bbe4f1a2dcca416e4b1c73fe8f0fd503aee
mark <markpackham1@gmail.com>
2022-12-02 11:25:09 +0000

    create bank account
    https://youtu.be/M5ugY7fWydE?t=23318

---
+++ ThreadLockExample/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadLockExample
+{
+    internal class BankAccount
+    {
+        private Object acctLock = new Object();
+        double Balance { get; set; }
+        string Name { get; set; }
+
+        public BankAccount(string name = "No Name", double bal = 1000)
+        {
+            Balance = bal;
+            Name = name;
+        }
+
+        public double Withdraw(double amt)
+        {
+            if ((Balance - amt) < 0)
+            {
+                Console.WriteLine($"Sorry ${Balance} in Account");
+                return Balance;
+            }
+
+            lock (acctLock)
+            {
+                if (Balance >= amt)
+                {
+                    Console.WriteLine($"Removed {amt} and {(Balance - amt)} left in Account");
+                    Balance -= amt;
+                }
+
+                return Balance;
+
+            }
+        }
+
+        public void IssueWithdraw()
+        {
+            Withdraw(1);
+        }
+    }
+}
