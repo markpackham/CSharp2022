d9c584b62bd7194193dbdc737fdbbf04cef4ddd2
mark <markpackham1@gmail.com>
2022-12-02 10:36:22 +0000

    demo use of sleep with thread
    https://youtu.be/M5ugY7fWydE?t=22982

---
+++ ThreadSleep/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ThreadSleep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(num);
+                Thread.Sleep(1000);
+                num++;
+            }
+            Console.WriteLine("Thread Ended");
+        }
+    }
+}
