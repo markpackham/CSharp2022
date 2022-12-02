4329623280bc7906bd7134c65b01c4081fd6aa67
mark <markpackham1@gmail.com>
2022-12-02 10:32:24 +0000

    show thread in action
    https://youtu.be/M5ugY7fWydE?t=22904

---
+++ ThreadExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadExamples
+{
+    internal class Program
+    {
+        static void Print1()
+        {
+            for(int i=0; i<1000; i++)
+            {
+                Console.Write(1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Print1);
+            // Threads share resources so you can never fully predict when one will start
+            t.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+        }
+    }
+}
