b34e7778a58e7c777ec3f0fe2db74f44392ce0ff
mark <markpackham1@gmail.com>
2022-12-02 08:40:13 +0000

    show only lightest animals
    https://youtu.be/M5ugY7fWydE?t=21719

--- LINQExamples2/Animal.cs
+++ LINQExamples2/Animal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LINQExamples2
+﻿namespace LINQExamples2
 {
     class Animal
     {
--- LINQExamples2/Program.cs
+++ LINQExamples2/Program.cs
@@ -12,9 +12,22 @@ namespace LINQExamples2
                 new Animal{Name = "Pongo", Height = 11, Weight = 11},
                 new Animal{Name = "Fifi", Height = 12, Weight = 12},
                 new Animal{Name = "Jasper", Height = 13, Weight = 13},
+                new Animal{Name = "Rover", Height = 14, Weight = 14},
+                new Animal{Name = "Dumbo", Height = 15, Weight = 15},
             };
 
             var famAnimalEnum = famAnimals.OfType<Animal>();
+
+            var smAnimals = from animal in famAnimalEnum
+                            where animal.Weight<=12
+                            orderby animal.Name
+                            select animal;
+
+            Console.WriteLine("Animals that weight 12k of less");
+            foreach (var animal in smAnimals )
+            {
+                Console.WriteLine($"{animal.Name} weights {animal.Weight}kg");
+            }
         }
     }
 }
