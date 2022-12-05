29d954f348fcba4fbec9f0a26943fcc0d5bcdbfa
mark <markpackham1@gmail.com>
2022-12-05 12:58:38 +0000

    use StreamWriter to write to file
    https://youtu.be/M5ugY7fWydE?t=24920

---
+++ FileStreamWriterReader/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileStreamWriterReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"E:\Sites\deleteMe\testFile3.txt";
+            StreamWriter sw = new StreamWriter(textFilePath);
+            sw.Write("This is a random ");
+            sw.Write(" yep 100% totally random ");
+            sw.WriteLine("sentance. With new line.");
+            sw.WriteLine("A new line has been written.");
+            sw.Close();
+        }
+    }
+}
