c5379c00bbbdc1e676dcbc292e37ef0e0e1842c1
mark <markpackham1@gmail.com>
2022-12-05 11:44:32 +0000

    get DirectoryInfo
    https://youtu.be/M5ugY7fWydE?t=24022

---
+++ DirectoriesFilesExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DirectoriesFilesExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currentDir = new DirectoryInfo(".");
+            DirectoryInfo markDir = new DirectoryInfo(@"C\:Users\mark");
+            Console.WriteLine(markDir.FullName);
+        }
+    }
+}
