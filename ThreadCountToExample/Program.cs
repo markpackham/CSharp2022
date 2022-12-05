f157bff552dd7c5faeb5b2d913d70433a1b74f78
mark <markpackham1@gmail.com>
2022-12-05 11:40:40 +0000

    demo counting with threads
    https://youtu.be/M5ugY7fWydE?t=23936

---
+++ ThreadCountToExample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadCountToExample
+{
+    internal class Program
+    {
+        static void CountTo(int maxNum = 1)
+        {
+            for(int i = 0; i < maxNum; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() => CountTo(10));
+            t.Start();
+
+            new Thread(() => { 
+            CountTo(5);
+            CountTo(6);
+            }).Start();
+        }
+    }
+}
