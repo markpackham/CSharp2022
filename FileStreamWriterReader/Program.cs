2451a73a54412f8627cfa7f584d3c3870e10ba9d
mark <markpackham1@gmail.com>
2022-12-05 13:04:04 +0000

    finish off StreamReader
    https://youtu.be/M5ugY7fWydE?t=25072

--- FileStreamWriterReader/Program.cs
+++ FileStreamWriterReader/Program.cs
@@ -10,7 +10,18 @@
             sw.Write(" yep 100% totally random ");
             sw.WriteLine("sentance. With new line.");
             sw.WriteLine("A new line has been written.");
+            sw.WriteLine("A new line has been written.");
+            sw.WriteLine("A new line has been written.");
+            sw.WriteLine("A new line has been written.");
             sw.Close();
+
+            StreamReader sr = new StreamReader(textFilePath);
+            // Peek: T
+            Console.WriteLine($"Peek : {Convert.ToChar(sr.Peek())}");
+            // 1st String This is a random  yep 100% totally random sentance. With new line.
+            Console.WriteLine($"1st String {sr.ReadLine()}");
+            Console.WriteLine($"Everything else: {sr.ReadToEnd()}");
+            sr.Close();
         }
     }
 }
