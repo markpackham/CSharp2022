248d9e4ba0625d06fec755bd7253aecc2a42d1e0
mark <markpackham1@gmail.com>
2022-12-05 07:59:31 +0000

    demo priority of threads
    https://youtu.be/M5ugY7fWydE?t=23746

--- ThreadLockExample/Program.cs
+++ ThreadLockExample/Program.cs
@@ -15,6 +15,15 @@
                 t.Name = i.ToString();
                 threads[i] = t;
             }
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Thread {threads[i].Name} Alive :{threads[i].Name}");
+                threads[i].Start();
+                Console.WriteLine($"Thread {threads[i].Name} Alive :{threads[i].Name}");
+            }
+            Console.WriteLine($"Current Priority {Thread.CurrentThread.Priority}");
+            Console.WriteLine($"Current Ending {Thread.CurrentThread.Name}");
         }
     }
 }
