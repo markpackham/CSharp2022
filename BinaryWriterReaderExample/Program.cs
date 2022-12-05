1b0d9685c4faa8c6b04b1a4a8bf6c3f8827275de
mark <markpackham1@gmail.com>
2022-12-05 14:01:06 +0000

    finish off BinaryReader and BinaryWriter example
    https://youtu.be/M5ugY7fWydE?t=25283

--- BinaryWriterReaderExample/Program.cs
+++ BinaryWriterReaderExample/Program.cs
@@ -7,7 +7,21 @@
             string textFilePath = @"E:\Sites\deleteMe\textFile4";
 
             // We use Binary Readers and Writers to handle data types
-            FileInfo datFil= new FileInfo(textFilePath);
+            FileInfo dataFile= new FileInfo(textFilePath);
+            BinaryWriter bw = new BinaryWriter(dataFile.OpenWrite());
+            string randomText = "Random string text";
+            int myAge = 20;
+            double height = 6.3;
+            bw.Write(randomText);
+            bw.Write(myAge);
+            bw.Write(height);
+            bw.Close();
+
+            BinaryReader br = new BinaryReader(dataFile.OpenRead());
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadDouble());
+            br.Close();
         }
     }
 }
