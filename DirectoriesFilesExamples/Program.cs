355d500d4f32d013afd725360db8361de880732f
mark <markpackham1@gmail.com>
2022-12-05 11:51:41 +0000

    demo directory creation and deletion
    https://youtu.be/M5ugY7fWydE?t=24195

--- DirectoriesFilesExamples/Program.cs
+++ DirectoriesFilesExamples/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             DirectoryInfo currentDir = new DirectoryInfo(".");
-            DirectoryInfo markDir = new DirectoryInfo(@"C\:Users\mark");
+            DirectoryInfo markDir = new DirectoryInfo(@"E:\Sites");
             Console.WriteLine(markDir.FullName);
+            Console.WriteLine(markDir.Name);
+            Console.WriteLine(markDir.Parent);
+            Console.WriteLine(markDir.Attributes);
+            Console.WriteLine(markDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"E:\Sites\deleteMe");
+            dataDir.Create();
+            //Directory.Delete(@"E:\Sites\deleteMe");
+            dataDir.Delete();
+
         }
     }
 }
