65de58b615c17cd101b9e1b9e2dbce78a4099331
mark <markpackham1@gmail.com>
2022-12-05 13:06:35 +0000

    create Binary Reader and Writer project
    https://youtu.be/M5ugY7fWydE?t=25099

---
+++ BinaryWriterReaderExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BinaryWriterReaderExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"E:\Sites\deleteMe\textFile4";
+
+            // We use Binary Readers and Writers to handle data types
+            FileInfo datFil= new FileInfo(textFilePath);
+        }
+    }
+}
