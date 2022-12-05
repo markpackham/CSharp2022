ab21194bb7e5ac9a6fc5f7ee83170894d1a7f47b
mark <markpackham1@gmail.com>
2022-12-05 12:46:39 +0000

    create file stream and carry out byte array conversion
    https://youtu.be/M5ugY7fWydE?t=24736

---
+++ CloudStreamFilePath/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace FileStreamExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFilePath = @"E:\Sites\deleteMe\testFile2.txt";
+            FileStream fs = File.Open(textFilePath, FileMode.Create);
+            string randString = "This is a random string";
+            // covner to byte array
+            byte[] rsByteArray = Encoding.Default.GetBytes(randString);
+
+            fs.Write(rsByteArray, 0, rsByteArray.Length);
+            fs.Position= 0;
+
+            byte[] fileByteArray = new byte[rsByteArray.Length];
+        }
+    }
+}
